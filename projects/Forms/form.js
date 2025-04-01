2bf452f7e74bf0c2262c9761b3a9c9d70e3b63df
Atoimoana676 <toimoanaomar@gmail.com>
2025-03-31 21:50:30 -1000

    Forms Project to server

---
+++ projects/Forms/form.js
@@ -0,0 +1,41 @@
+document.getElementById('myForm').addEventListener('submit',function(event){
+    event.preventDefault();
+    alert("Form Submitted");
+
+    const Fullname = document.getElementById('Fullname').value;
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('pass').value;
+    const age = document.getElementById('age').value;
+    
+    if (!Fullname) {
+        alert("you need to enter your name.");
+        return;
+    }
+
+    if (!age || age<18) {
+        alert("you need to be 18");
+        return;
+    
+    }
+
+    const formData = {
+        Fullname: Fullname,
+        email: email,
+        password: pass,
+    }
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "submit.json", true);
+    xhr.setRequestHeader("content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById('message').innerHTML = response.message;
+            document.getElementById('myForm').innerHTML = "";
+        } else if (xhr.readyState === 4) {
+            alert('Error submitting form.');
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+    console.log(formData);
+});
